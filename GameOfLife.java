45741463443b02f2e8e32f1cfdcd78592ab397d1
Maayan <maayannehama.crystal@post.runi.ac.il>
2024-01-19 12:08:11 +0200

    friday12

--- GameOfLife.java
+++ GameOfLife.java
@@ -153,13 +153,9 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		for(int i = 1 ; i < arr.length -1 ; i++){
 			for(int j = 1 ; j < arr[0].length -1 ; j++){
-				if(j < arr[0].length -2){
-					System.out.print(arr[i][j] + " ");
+					System.out.printf("%4s", arr[i][j]);
 				}
-				else {
-					System.out.print(arr[i][j]);
-				}
-			}
+			
 			System.out.println();
 		}
 	}
