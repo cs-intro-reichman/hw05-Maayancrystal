2623f35c035c4b5394a4cf77fe6c8215f3bad30e
Maayan <maayannehama.crystal@post.runi.ac.il>
2024-01-19 11:53:50 +0200

    friday11

--- GameOfLife.java
+++ GameOfLife.java
@@ -11,10 +11,10 @@ public class GameOfLife {
 		String fileName = args[0];
 		//// Uncomment the test that you want to execute, and re-compile.
 		//// (Run one test at a time).
-		 ///test1(fileName);
+		 test1(fileName);
 		///test2(fileName);
 		 ///test3(fileName, 3);
-		 play(fileName);
+		 ///play(fileName);
 	}
 	
 	// Reads the data file and prints the initial board.
@@ -153,7 +153,7 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		for(int i = 1 ; i < arr.length -1 ; i++){
 			for(int j = 1 ; j < arr[0].length -1 ; j++){
-				System.out.print(arr[i][j] + "");
+				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
 		}
